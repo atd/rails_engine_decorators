40c3d42ea3dd2f70f96599cb4fa7a4371a86ea3a
Peter Wagenet <peter.wagenet@gmail.com>
2016-07-27 21:47:11 -0700

    Fix Rails 5 alias_method_chain deprecation

--- lib/rails_engine_decorators/engine/configuration.rb
+++ lib/rails_engine_decorators/engine/configuration.rb
@@ -1,13 +1,12 @@
 require 'rails/engine/configuration'
 
 Rails::Engine::Configuration.class_eval do
-  def paths_with_decorators
+  alias paths_without_decorators paths
+  def paths
     @paths ||= begin
       paths = paths_without_decorators
       paths.add "app/decorators", :eager_load => true
       paths
     end
   end
-
-  alias_method_chain :paths, :decorators
 end
